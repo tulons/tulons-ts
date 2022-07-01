37f2c8ffbaf1a350a06d27c66bff37adc1710661
Le Yu <leyu00@gmail.com>
2022-07-01 14:10:09 -0400

    Replace fetchMock with mockAdapter

--- tests/tulons.test.ts
+++ tests/tulons.test.ts
@@ -1,28 +1,24 @@
 /* eslint-disable @typescript-eslint/unbound-method */
+import axios from "axios";
 import { Tulons } from "../src/tulons";
+import MockAdapter from "axios-mock-adapter";
 
-import mockFetch, { MockResponseInit } from 'jest-fetch-mock'
-
-mockFetch.enableMocks();
+// This sets the mock adapter on the default instance
+const mock = new MockAdapter(axios);
 
 test("Can get Genesis from wallet address", async () => {
 
-  const tulons = new Tulons('localhost', 1);
-
+  const tulons = new Tulons("localhost", 1);
+  
   // set mock for the streams expected request
-  fetchMock.mockIf(/^localhost\/api\/v0\/streams$/, (req: Request): Promise<MockResponseInit | string> => {
-
-    return Promise.resolve({
-      "body": JSON.stringify({
-        "streamid": "k2t6wy...",
-        "state": {
-          "content": {
-            "kz2832...": "ceramic://kz29j...",
-            "k3qp34...": "ceramic://kz30x..."
-          }
-        }
-      })
-    });
+  mock.onPost("localhost/api/v0/streams").reply(200, {
+    "streamid": "k2t6wy...",
+    "state": {
+      "content": {
+        "kz2832...": "ceramic://kz29j...",
+        "k3qp34...": "ceramic://kz30x..."
+      }
+    }
   });
 
   const genesis = await tulons.getGenesis("0x0");
@@ -40,17 +36,12 @@ test("Can get Stream from a streamId", async () => {
   const tulons = new Tulons('localhost', 1);
   
   // set mock for the streams expected request
-  fetchMock.mockIf(/^localhost\/api\/v0\/multiqueries$/, (req: Request): Promise<MockResponseInit | string> => {
-
-    return Promise.resolve({
-      "body": JSON.stringify({
-        "kz29j...": {
-          "content": {
-            "test": "value"
-          }
-        }
-      })
-    });
+  mock.onPost("localhost/api/v0/multiqueries").reply(200, {
+    "kz29j...": {
+      "content": {
+        "test": "value"
+      }
+    }
   });
 
   const content = await tulons.getStream("ceramic://kz29j...");
@@ -64,17 +55,12 @@ test("Can get Streams from an array of streamIds", async () => {
   const tulons = new Tulons('localhost', 1);
   
   // set mock for the streams expected request
-  fetchMock.mockIf(/^localhost\/api\/v0\/multiqueries$/, (req: Request): Promise<MockResponseInit | string> => {
-
-    return Promise.resolve({
-      "body": JSON.stringify({
-        "kz29j...": {
-          "content": {
-            "test": 1
-          }
-        }
-      })
-    });
+  mock.onPost("localhost/api/v0/multiqueries").reply(200, {
+    "kz29j...": {
+      "content": {
+        "test": 1
+      }
+    }
   });
 
   const content = await tulons.getStreams(["ceramic://kz29j..."]);
@@ -90,17 +76,12 @@ test("Can get hydrated content from streams content", async () => {
   const tulons = new Tulons('localhost', 1);
 
   // set mock for the streams expected request
-  fetchMock.mockIf(/^localhost\/api\/v0\/multiqueries$/, (req: Request): Promise<MockResponseInit | string> => {
-
-    return Promise.resolve({
-      "body": JSON.stringify({
-        "kz29j...": {
-          "content": {
-            "test": 1
-          }
-        }
-      })
-    });
+  mock.onPost("localhost/api/v0/multiqueries").reply(200, {
+    "kz29j...": {
+      "content": {
+        "test": 1
+      }
+    }
   });
 
   const content = await tulons.getHydrated({
@@ -118,23 +99,18 @@ test("Can get hydrated content from streams content recursively", async () => {
   const tulons = new Tulons('localhost', 1);
   
   // set mock for the streams expected request
-  fetchMock.mockIf(/^localhost\/api\/v0\/multiqueries$/, (req: Request): Promise<MockResponseInit | string> => {
-
-    return Promise.resolve({
-      "body": JSON.stringify({
-        "kz29j...": {
-          "content": {
-            "test": "ceramic://kz452..."
-          }
-        },
-        // this would be returned in a second query to /api/v0/multiqueries
-        "kz452...": {
-          "content": {
-            "test": 1
-          }
-        }
-      })
-    });
+  mock.onPost("localhost/api/v0/multiqueries").reply(200, {
+    "kz29j...": {
+      "content": {
+        "test": "ceramic://kz452..."
+      }
+    },
+    // this would be returned in a second query to /api/v0/multiqueries
+    "kz452...": {
+      "content": {
+        "test": 1
+      }
+    }
   });
 
   const content = await tulons.getHydrated({
